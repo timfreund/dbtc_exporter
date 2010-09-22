5291b17f3f145b01e8a0bde1b6a5f62c3e4c5bc8
Tim Freund <tim@freunds.net>
2010-09-21 21:48:31 -0500

    initial csv exporter

--- dbtc_exporter.py
+++ dbtc_exporter.py
@@ -25,15 +25,21 @@ class DBTCExporter(object):
         self.logged_in = False
 
     def login(self):
-        browser.go("http://dontbreakthechain.com/accounts/login")
-        f = browser.get_all_forms()[0]
+        self.browser.go("http://dontbreakthechain.com/accounts/login")
+        f = self.browser.get_all_forms()[0]
         f.set_value(value=self.username, name="username")
         f.set_value(value=self.password, name="password")
-        browser._browser.form = f 
-        browser.submit("login")
-        self.logged_in = True
-        
+        self.browser._browser.form = f 
+        self.browser.submit("login")
+        self.logged_in = True        
         # TODO confirm login here
+
+    def retrieve_all_data(self):
+        chains = self.retrieve_chains()
+        for chain in chains:
+            chain_data = self.retrieve_chain_data(chain['id'])
+            chain['data'] = chain_data
+        return chains
 
     def retrieve_chains(self):
         if not self.logged_in:
@@ -43,63 +49,72 @@ class DBTCExporter(object):
         chain_list = chain_soup.find(attrs={'id': 'chains'})
         chain_links = chain_list.findAll(attrs={'class': 'chain'})
     
-        self.chains  = []
+        chains  = []
         for chain_link in chain_links:
             chain = {}
             chain['name'] = chain_link.renderContents().strip()
             chain['id'] = chain_link.attrMap['href'].split('/')[-1]
 
-            self.chains.append(chain)
+            chains.append(chain)
+        return chains
 
-    def retrieve_chain_data(self, chain_name):
-        target = None
-        for chain in self.chains:
-            if chain['name'] == chain_name:
-                target = chain
-        
-        if target is None:
-            raise Exception("No such chain")
+    def retrieve_chain_data(self, chain_id):
+        chain_data = {}
 
-        self.browser.go("blah/blah/%s" % target['id'])
+        self.browser.go("http://dontbreakthechain.com/year/%s" % chain_id)
         chain_soup = BeautifulSoup(self.browser.get_html())
         days = chain_soup.findAll(attrs={'class': 'day day-hover link'})
         for day in days:
-            print day.attrMap['id'].split('_')[-1]
+            date = day.attrMap['id'].split('_')[-1]
+            chain_data[date] = True
+
+        return chain_data
 
 
 if __name__ == "__main__":
-    html_file = open("40783.html")
-    html = html_file.read()
-    html_file.close()
+    # parser = create_arg_parser()
+    # (options, args) = parser.parse_args()
 
-    chains = []
+    # if None in [options.user, options.password]:
+    #     print "Username and password is required" 
+    #     arg_parser.print_help()
+    #     sys.exit(-1)
 
-    # chain_doc = ElementTree(file="40783.html")
-    chain_soup = BeautifulSoup(html)
-    chain_list = chain_soup.find(attrs={'id': 'chains'})
-    chain_links = chain_list.findAll(attrs={'class': 'chain'})
+    # exporter = DBTCExporter(parser.user, parser.password)
+    # data = exporter.retrieve_all_data()
+
+
+    import json
+    decoder = json.JSONDecoder()
+    chains = decoder.decode(open("dbtc_data.json", "r").read())
+
+    import csv
+    import datetime
+
+    header = ["date"]
+    end_date = datetime.datetime.now()
+    start_date = None
+    for chain in chains:
+        header.append(chain['name'])
+        chain_start_date = min(chain['data'].keys())
+        if start_date is None or chain_start_date < start_date:
+            start_date = chain_start_date
+
+    output_file = open("dbtc_export.csv", "w")
+    csv_writer = csv.writer(output_file)
+    csv_writer.writerow(header)
+    day = datetime.timedelta(1)
+    date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
     
-    for chain_link in chain_links:
-        chain = {}
-        chain['name'] = chain_link.renderContents().strip()
-        chain['id'] = chain_link.attrMap['href'].split('/')[-1]
+    while(date < end_date):
+        date_str = date.strftime("%Y-%m-%d")
+        row = [date_str]
+        for chain in chains:
+            if chain['data'].has_key(date_str):
+                row.append('X')
+            else:
+                row.append('')
+        csv_writer.writerow(row)
+        date = date + day
 
-        chains.append(chain)
-
-    print chains
-
-    days = chain_soup.findAll(attrs={'class': 'day day-hover link'})
-    for day in days:
-        print day.attrMap['id'].split('_')[-1]
-
-
-
-    b = get_browser()
-    b.go("http://dontbreakthechain.com/accounts/login")
-    f = b.get_all_forms()[0]
-    f.set_value(value="my_username", name="username")
-    f.set_value(value="my_password", name="password")
-    b._browser.form = f 
-    b.submit("login")
-    html = b.get_html()
-
+    output_file.close()
